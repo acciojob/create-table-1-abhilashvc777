b68ff26aaf9b5cd88a7f99f1a1f12fbd270d299b
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-28 15:51:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function insert_Row() {
-    //Write your code heredocument.querySelectorAll("#sampleTable tr")
-  const tr1= document.createElement("tr")
-  // const table = document.querySelector("#sampleTable")
-  const tr = document.querySelectorAll("#sampleTable tr")
-  // tr1.setAttribute("id","tr1")
-  const insert = `<td>New cell1</td> 
-		         <td>New cell2</td>`
-	tr1.innerHTML = insert
-	const parent = tr[0].parentNode
-	parent.insertBefore(tr1,tr[0])
+    const tr = document.querySelectorAll("#sampleTable tr")
+    const tr1= document.createElement("tr")
+
+    const td1 = document.createElement("td")
+    td1.textContent = "New Cell1"
+    const td2 = document.createElement("td")
+    td2.textContent = "New Cell2"
+
+    tr1.appendChild(td1)
+    tr1.appendChild(td2)
+
+    const parent = tr[0].parentNode
+    parent.insertBefore(tr1,tr[0])
 }
